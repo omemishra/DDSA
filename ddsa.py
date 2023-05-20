99bbf100fca635002d284cdd5a08b81e64e8620b
Ome Mishra <omemishra@users.noreply.github.com>
2023-05-20 06:41:01 +0530

    Update ddsa.py
    It will check for JWT as well

--- ddsa.py
+++ ddsa.py
@@ -36,12 +36,12 @@ with open(output_file, "w") as file:
 
 print(f"Data fetched and stored in '{output_file}'")
 
-# Prompt user to select the desired options
 print("Select the options to process the fetched data:")
 print("1. Search for emails")
 print("2. Search for sessions")
 print("3. Search for API keys")
-print("4. Search for other specified data")
+print("4. Search for JWT tokens")
+print("5. Search for other specified data")
 
 # Process the selected options
 selected_options = input("Enter the option numbers (separated by commas): ")
@@ -59,7 +59,7 @@ if 1 in selected_options:
     for email in filtered_emails:
         urls_with_email = [url for url in re.findall(r"https?://[^\s/$.?#].[^\s]*", output) if email in url]
         print(Fore.GREEN + f"Email: {email}")
-        print(Fore.RED + "Found in:")
+        print(Fore.RED + "Found in URL:")
         print("\n".join(urls_with_email))
         print()
 
@@ -67,19 +67,51 @@ if 1 in selected_options:
 if 2 in selected_options:
     print("Sessions found:")
     sessions = re.findall(r"\b[A-Fa-f0-9]{64}\b", output)
-    print("\n".join(sessions))
+    for session in sessions:
+        urls_with_session = [url for url in re.findall(r"https?://[^\s/$.?#].[^\s]*", output) if session in url]
+        print(Fore.GREEN + f"Sessions Found: {session}")
+        print(Fore.RED + "Found in URL:")
+        print("\n".join(urls_with_session))
+        print()
+
+
 
 # Search for API keys
 if 3 in selected_options:
     print("API keys found:")
     apikeys = re.findall(r"\b[A-Za-z0-9]{32}\b", output)
-    print("\n".join(apikeys))
+    for key in apikeys:
+        urls_with_key = [url for url in re.findall(r"https?://[^\s/$.?#].[^\s]*", output) if key in url]
+        print(Fore.GREEN + f"API Key: {key}")
+        print(Fore.RED + "Found in URL:")
+        print("\n".join(urls_with_key))
+        print()
 
-# Search for other specified data
+
 if 4 in selected_options:
+    print("JWT tokens found:")
+    jwt_tokens = re.findall(r"eyJh\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\b", output)
+    for jwt in jwt_tokens:
+        urls_with_jwt = [url for url in re.findall(r"https?://[^\s/$.?#].[^\s]*", output) if jwt in url]
+        print(Fore.GREEN + f"JWT Token: {jwt}")
+        print(Fore.RED + "Found in URL:")
+        print("\n".join(urls_with_jwt))
+        print()
+
+
+if 5 in selected_options:
     other_data = input("Enter the patterns to search (separated by commas): ")
     patterns = [pattern.strip() for pattern in other_data.split(",")]
     for pattern in patterns:
         print(f"{pattern} found:")
         matches = re.findall(pattern, output, re.IGNORECASE)
-        print("\n".join(matches))
+        urls_with_pattern = [url for url in re.findall(r"https?://[^\s/$.?#].[^\s]*", output) if any(pattern in url for pattern in matches)]
+        if urls_with_pattern:
+            print("Found in URLs:")
+            print("\n".join(urls_with_pattern))
+
+
+
+
+
+
